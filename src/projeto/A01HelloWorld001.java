5d7cf050e9591fe986e0129fde3368d576cded8f
danielnascimentotomaz <danielsabio1000@hotmail.com>
2022-06-17 06:43:05 -0300

    "INICIO"

---
+++ src/projeto/A01HelloWorld001.java
@@ -0,0 +1,13 @@
+/*
+* O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!".
+*  Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
+
+* */
+
+package projeto;
+
+public class A01HelloWorld001 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
